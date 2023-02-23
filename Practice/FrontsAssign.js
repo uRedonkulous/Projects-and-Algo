aad5f114e64bc1f29047e869253fedad6d977968
uRedonkulous <kaitoyomma@yahoo.com>
2023-02-22 21:12:14 -0700

    Fronts Assignment

--- Practice/FrontsAssign.js
+++ Practice/FrontsAssign.js
@@ -3,10 +3,6 @@
 // Use only a single JavaScript file for this assignment. 
 // All examples are done in order, starting with a new instance of the SLL class.
 
-// Add Front
-// Write a method that accepts a value and create a new node, 
-// assign it to the list head, and return a pointer to the new head node.
-
 class SLLNode { //Singly Linked List class
     constructor(data, nextNode = null) {
         this.data = data; //Holds data
@@ -18,13 +14,20 @@ class SLL { // Singly Linked List
     constructor() {
         this.head = null; // Head pointing to first node
     }
- // Methods
+// Add Front
+// Write a method that accepts a value and create a new node, 
+// assign it to the list head, and return a pointer to the new head node.
+
     addFront(value) {
         let newNode = new SLLNode(value)
             newNode.next = this.head;
             this.head = newNode; //Points to new head
             return this;
         }
+
+// Remove Front
+// Write a method to remove the head node and return the new list head node. If the list is empty, return null.
+
     removeFront() { //Remove first number at head of SLL
         if(this.head == null) {
             return this;
@@ -34,6 +37,10 @@ class SLL { // Singly Linked List
         removeNode.next = null;
         return this;
     }
+
+// Front
+// Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
+
     front() { //Display number at front of SLL
         if (this.head == null) { //If list is empty
             return null;
@@ -48,8 +55,9 @@ class SLL { // Singly Linked List
     // SLL1.addFront(73);
     // console.log(SLL1.front());
     // console.log(SLL1);
-    // SLL1.addFront(18);
-    // SLL1.addFront(5);
+    // SLL1.addFront(18) // => Node { data: 18, next: null }
+    // SLL1.addFront(5) // => Node { data: 5, next: Node { data: 18, next: null } }
+    // SLL1.addFront(73) // => Node { data: 73, next: Node { data: 5, next: Node { data: 18, next: null } } }
     // SLL1.removeFront(); // => Node { data: 5, next: Node { data: 18, next: null } }
     // SLL1.removeFront(); // => Node { data: 18, next: null }
     console.log(SLL1);
@@ -63,12 +71,6 @@ class SLL { // Singly Linked List
     // Examples:
 
 // SLL1 = new SLL()
-// SLL1.addFront(18) => Node { data: 18, next: null }
-// SLL1.addFront(5) => Node { data: 5, next: Node { data: 18, next: null } }
-// SLL1.addFront(73) => Node { data: 73, next: Node { data: 5, next: Node { data: 18, next: null } } }
-
-// Remove Front
-// Write a method to remove the head node and return the new list head node. If the list is empty, return null.
 
 // // Examples:
 // let SLL1 = new SLL();
@@ -78,8 +80,6 @@ class SLL { // Singly Linked List
 // SLL1.removeFront(); // => Node { data: 5, next: Node { data: 18, next: null } }
 // SLL1.removeFront(); // => Node { data: 18, next: null }
 // console.log(SLL1)
-// Front
-// Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
 
 // Examples:
 
