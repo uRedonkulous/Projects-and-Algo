be2bfb6bcb22f01950a3062c6fe9cc65d6f6e2de
uRedonkulous <kaitoyomma@yahoo.com>
2023-02-23 19:52:19 -0700

    New Project

---
+++ Python/Practice/NewProject/flask_app/models/cards.py
@@ -0,0 +1,9 @@
+class Cards:
+
+    def __init__(self,data):
+        self.id = data['id']
+        self.name = data['name']
+        self.description = data['description']
+        self.skill1 = data['skill1']
+        self.skill2 = data['skill2']
+        self.skill3 = data['skill3']
