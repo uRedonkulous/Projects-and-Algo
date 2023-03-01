eef6a786c75327b7d28892ac8444d64c5f6a4436
uRedonkulous <kaitoyomma@yahoo.com>
2023-02-28 17:41:02 -0700

    Display Assign

--- Practice/FrontsAssign.js
+++ Practice/FrontsAssign.js
@@ -48,26 +48,59 @@ class SLL { // Singly Linked List
             return this.head.value;
         }
     }
+
+// Display
+// Use classes, attributes, and methods in the JavaScript language for this challenge. Use only a single JavaScript file for this assignment, it may be the same from the Fronts assignment. All examples are done in order, starting with a new instance of the SLL class.
+
+// Create display() that uses a while loop and a runner to return a string containing all list values. Build what you wish console.log(myList) did!
+
+    display() {
+        //Runner
+        let runner = this.head;
+        //Create an empty string
+        let listStr = "";
+        while (runner !== null) {
+            listStr += runner.data;
+            // Runs through code and appends a comma and space after data
+            if (runner.next !== null) {
+                listStr += ", ";
+            }
+            runner = runner.next;
+        }
+        // Return new string
+        return listStr;
     }
-    let SLL1 = new SLL(); //Create new empty SLL
-    SLL1.addFront(18);
-    SLL1.addFront(5);
-    // SLL1.addFront(73);
-    // console.log(SLL1.front());
+}
+    var SLL1 = new SLL(); //Create new empty SLL
+    // SLL1.addFront(18);
+    // SLL1.addFront(5);
+    // // SLL1.addFront(73);
+    // // console.log(SLL1.front());
+    // // console.log(SLL1);
+    SLL1.addFront(18) // => Node { data: 18, next: null }
+     SLL1.addFront(5) // => Node { data: 5, next: Node { data: 18, next: null } }
+     SLL1.addFront(73) // => Node { data: 73, next: Node { data: 5, next: Node { data: 18, next: null } } }
+    // // SLL1.removeFront(); // => Node { data: 5, next: Node { data: 18, next: null } }
+    // // SLL1.removeFront(); // => Node { data: 18, next: null }
+    console.log(SLL1);
+    // SLL1.front() // => 18
+    // // console.log(SLL1);
     // console.log(SLL1);
-    // SLL1.addFront(18) // => Node { data: 18, next: null }
-    // SLL1.addFront(5) // => Node { data: 5, next: Node { data: 18, next: null } }
-    // SLL1.addFront(73) // => Node { data: 73, next: Node { data: 5, next: Node { data: 18, next: null } } }
-    // SLL1.removeFront(); // => Node { data: 5, next: Node { data: 18, next: null } }
-    // SLL1.removeFront(); // => Node { data: 18, next: null }
-    console.log(SLL1);
-    SLL1.front() // => 18
+    // SLL1.removeFront() // => null
     // console.log(SLL1);
-    console.log(SLL1);
-    SLL1.removeFront() // => null
-    console.log(SLL1);
-    SLL1.front() // => null
-    console.log(SLL1);
+    // SLL1.front() // => null
+    console.log(SLL1.display());
+
+    // SLL1 = new SLL()
+    // SLL1.addFront(76); //=> Node { data: 76, next: null }
+    // console.log(SLL1.display());
+    // SLL1.addFront(2);// => Node { data: 2, next: Node { data: 76, next: null } }
+    // // SLL1.display() //=> "2, 76"
+    // SLL1.addFront(11.41); //=> Node { data: 11.41, next: Node { data: 2, next: Node { data: 76, next: null } } }
+    // console.log(SLL1.display()) //=> "11.41, 2, 76"
+
+
+
     // Examples:
 
 // SLL1 = new SLL()
@@ -86,3 +119,4 @@ class SLL { // Singly Linked List
 // SLL1.front() // => 18
 // SLL1.removeFront() // => null
 // SLL1.front() // => null
+
